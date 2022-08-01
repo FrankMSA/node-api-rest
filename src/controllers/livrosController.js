88911640f78988ffa1dfa12e945d71f8ddc7581b
Franklin Montijo Salviano de Almeida <82833834+FrankMSA@users.noreply.github.com>
2022-08-01 12:40:51 -0300

    CRUD pronto

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -5,6 +5,16 @@ class LivroController {
         livros.find((err, livros) => {
             res.status(200).json(livros)
     })}
+    static listarLivroPorId = (req, res) => {
+        const id = req.params.id;
+        livros.findById(id, (err, livros) => {
+            if(err) {
+                res.status(400).send({message: `${err.message} - ID do livro não localizada.`});
+            } else {
+                res.status(200).send(livros);
+            }
+        })
+    }
     static cadastrarLivro = (req, res) => {
         let livro = new livros(req.body);
         livro.save((err) => {
@@ -14,6 +24,26 @@ class LivroController {
                 res.status(201).send(livro.toJSON())
             }
         })
+    }
+    static atualizarLivro = (req, res) => {
+        const id = req.params.id;
+        livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(!err) {
+                res.status(200).send({message: 'Livro atualizado com sucesso!'})
+            } else {
+                res.status(500).send({message: err.message})
+            }
+        })
+    }
+    static excluirLivro = (req, res) => {
+        const id = req.params.id;
+        livros.findByIdAndDelete(id, (err) => {
+            if(!err) {
+                res.status(200).send({message: 'Livro removido com sucesso!'})
+            } else {
+                res.status(500).send({message: err.message})
+            }
+        });
     }
 }
 
