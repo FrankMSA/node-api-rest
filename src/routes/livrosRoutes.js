38e3a2f6e14442598e6792523690e5319cc6d637
Franklin Montijo Salviano de Almeida <82833834+FrankMSA@users.noreply.github.com>
2022-08-02 09:13:19 -0300

    API REST COMPLETA

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router
     .get("/livros", LivroController.listarLivros)
+    .get("/livros/busca", LivroController.listarLivroPorEditora) //A busca mais complexa vem antes da busca por id por prioridade
     .get("/livros/:id", LivroController.listarLivroPorId)
     .post("/livros", LivroController.cadastrarLivro)
     .put("/livros/:id", LivroController.atualizarLivro)
